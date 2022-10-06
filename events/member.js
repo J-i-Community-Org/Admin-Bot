062a6e3debc2594e0ae94846485fce19aaef96f4
ericchase <github@eric.addymail.com>
2022-10-05 20:33:30 -0500

    add: preliminary code
    Signed-off-by: ericchase <github@eric.addymail.com>

---
+++ events/member.js
@@ -0,0 +1,11 @@
+const { Client } = require("discord.js");
+const client = require("../index.js");
+const config = require("../util/conf/botconfig.json");
+
+client.on("guildMemberAdd", async (member, guild) => {
+  if (`${config.ServerBlacklisted}`.includes(member.id)) {
+    // If we wanna add an automatic quarantining of a blacklisted user itll go here
+  }
+});
+
+client.on("guildMemberRemove", async (member, guild) => {});
